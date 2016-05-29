2cc7681824ed7e001318ac6cc2234a46a3b45852
Lars Haugseth <git@larshaugseth.com>
2016-05-29 04:38:45 +0200

    Refactored palettes into exported vars.

--- video.go
+++ video.go
@@ -5,7 +5,6 @@ package video
 
 import (
 	"encoding/hex"
-	"fmt"
 	"image/color"
 	"strings"
 )
@@ -30,16 +29,17 @@ const (
 	LightGrey
 )
 
-// Palette looks up a color set by name and returns an array of Color
-// values corresponding to each 16 colors in the set.
-// Valid names are: "pepto", "levy", "vice" and "vice-old"
-func Palette(name string) []color.Color {
-	hexValues, ok := paletteMap[name]
-	if !ok {
-		panic(fmt.Sprintf("breadbox/video: Invalid palette name %q", name))
-	}
+// Palettes each consisting of an array of 16 Color values
+var (
+	Pepto   = palette("000000:ffffff:68372b:70a4b2:6f3d86:588d43:352879:b8c76f:6f4f25:433900:9a6759:444444:6c6c6c:9ad284:6c5eb5:959595")
+	Levy    = palette("040204:fcfefc:cc3634:84f2dc:cc5ac4:5cce34:4436cc:f4ee5c:d47e34:945e34:fc9a94:5c5a5c:8c8e8c:9cfe9c:74a2ec:c4c2c4")
+	Vice    = palette("000000:fdfefc:be1a24:30e6c6:b41ae2:1fd21e:211bae:dff60a:b84104:6a3304:fe4a57:424540:70746f:59fe59:5f53fe:a4a7a2")
+	ViceOld = palette("000000:d5d5d5:72352c:659fa6:733a91:568d35:2e237d:aeb75e:774f1e:4b3c00:9c635a:474747:6b6b6b:8fc271:675db6:8f8f8f")
+)
+
+func palette(values string) []color.Color {
 	colors := make([]color.Color, 16)
-	for i, value := range strings.Split(hexValues, ":") {
+	for i, value := range strings.Split(values, ":") {
 		colors[i] = hexColor(value)
 	}
 	return colors
@@ -52,9 +52,3 @@ func hexColor(value string) color.Color {
 	}
 	return color.RGBA{rgb[0], rgb[1], rgb[2], 255}
 }
-
-var paletteMap = map[string]string{
-	"pepto":    "000000:ffffff:68372b:70a4b2:6f3d86:588d43:352879:b8c76f:6f4f25:433900:9a6759:444444:6c6c6c:9ad284:6c5eb5:959595",
-	"levy":     "040204:fcfefc:cc3634:84f2dc:cc5ac4:5cce34:4436cc:f4ee5c:d47e34:945e34:fc9a94:5c5a5c:8c8e8c:9cfe9c:74a2ec:c4c2c4",
-	"vice":     "000000:fdfefc:be1a24:30e6c6:b41ae2:1fd21e:211bae:dff60a:b84104:6a3304:fe4a57:424540:70746f:59fe59:5f53fe:a4a7a2",
-	"vice-old": "000000:d5d5d5:72352c:659fa6:733a91:568d35:2e237d:aeb75e:774f1e:4b3c00:9c635a:474747:6b6b6b:8fc271:675db6:8f8f8f"}
