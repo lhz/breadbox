c6f3291b5c5ba2fde20bb1aa8dfbf043f37459fb
Lars Haugseth <git@larshaugseth.com>
2016-05-31 18:24:09 +0200

    Added snapshot tool to file package.

---
+++ file/snapshot.go
@@ -0,0 +1,47 @@
+package file
+
+import (
+	"io/ioutil"
+	"bytes"
+)
+
+type Snapshot struct {
+	content []byte
+	c64mem  int
+}
+
+func NewSnapshot(source string) *Snapshot {
+	content, err := ioutil.ReadFile(source)
+	if err != nil {
+		panic(err)
+	}
+
+	header := bytes.Index(content, []byte("VICE Snapshot File"))
+	if header != 0 {
+		panic("No snapshot header found in source file.")
+	}
+
+	c64mem := bytes.Index(content, []byte("C64MEM"))
+	if c64mem < 0 {
+		panic("No C64MEM marker found in source file.")
+	}
+
+	return &Snapshot{content, c64mem + 26}
+}
+
+func (s *Snapshot) Inject(filename string) {
+	content, err := ioutil.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	address := int(content[0]) + 256 * int(content[1])
+	for i := 0; i < len(content) - 2; i++ {
+		s.content[s.c64mem + address + i] = content[2 + i]
+	}
+	return
+}
+
+func (s *Snapshot) WriteFile(target string) {
+	ioutil.WriteFile(target, s.content, 0644)
+}
