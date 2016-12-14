594e65b7d153b03dd46ba9466158940dfa8c362a
Lars Haugseth <git@larshaugseth.com>
2016-12-14 15:54:42 +0100

    Added Colodore default (50/100/50) palette.

--- gfx/palette.go
+++ gfx/palette.go
@@ -31,10 +31,11 @@ const (
 
 // Palettes each consisting of an array of 16 Color values
 var (
-	Pepto   = palette("000000:ffffff:68372b:70a4b2:6f3d86:588d43:352879:b8c76f:6f4f25:433900:9a6759:444444:6c6c6c:9ad284:6c5eb5:959595")
-	Levy    = palette("040204:fcfefc:cc3634:84f2dc:cc5ac4:5cce34:4436cc:f4ee5c:d47e34:945e34:fc9a94:5c5a5c:8c8e8c:9cfe9c:74a2ec:c4c2c4")
-	Vice    = palette("000000:fdfefc:be1a24:30e6c6:b41ae2:1fd21e:211bae:dff60a:b84104:6a3304:fe4a57:424540:70746f:59fe59:5f53fe:a4a7a2")
-	ViceOld = palette("000000:d5d5d5:72352c:659fa6:733a91:568d35:2e237d:aeb75e:774f1e:4b3c00:9c635a:474747:6b6b6b:8fc271:675db6:8f8f8f")
+	Colodore = palette("000000:ffffff:813338:75cec8:8e3c97:56ac4d:2e2c9b:edf171:8e5029:553800:c46c71:4a4a4a:7b7b7b:a9ff9f:706deb:b2b2b2")
+	Pepto    = palette("000000:ffffff:68372b:70a4b2:6f3d86:588d43:352879:b8c76f:6f4f25:433900:9a6759:444444:6c6c6c:9ad284:6c5eb5:959595")
+	Levy     = palette("040204:fcfefc:cc3634:84f2dc:cc5ac4:5cce34:4436cc:f4ee5c:d47e34:945e34:fc9a94:5c5a5c:8c8e8c:9cfe9c:74a2ec:c4c2c4")
+	Vice     = palette("000000:fdfefc:be1a24:30e6c6:b41ae2:1fd21e:211bae:dff60a:b84104:6a3304:fe4a57:424540:70746f:59fe59:5f53fe:a4a7a2")
+	ViceOld  = palette("000000:d5d5d5:72352c:659fa6:733a91:568d35:2e237d:aeb75e:774f1e:4b3c00:9c635a:474747:6b6b6b:8fc271:675db6:8f8f8f")
 )
 
 func palette(values string) []color.Color {
