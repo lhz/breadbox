0fef4d84fc38139f6f54848502c9f6873c91ac39
Lars Haugseth <git@larshaugseth.com>
2016-12-15 10:44:02 +0100

    Added flag parsing to hires2png.

--- cmd/hires2png.go
+++ cmd/hires2png.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/lhz/breadbox/gfx"
 
+	"flag"
 	"fmt"
 	"image"
 	"image/png"
@@ -15,13 +16,36 @@ var palette = gfx.Pepto
 var mask = []byte{128, 64, 32, 16, 8, 4, 2, 1}
 var c byte
 
+func usage() {
+	fmt.Fprintf(os.Stderr, "Usage: %v [flags] <source> <target>\n", os.Args[0])
+	flag.PrintDefaults()
+	os.Exit(1)
+}
+
 func main() {
-	if len(os.Args) != 3 {
-		fmt.Fprintf(os.Stderr, "Usage: %v <source> <target>\n", os.Args[0])
-		os.Exit(1)
+	paletteName := flag.String("p", "pepto", "Name of palette to use [pepto|colodore|vice|levy]")
+
+	flag.Parse()
+
+	if len(flag.Args()) != 2 {
+		usage()
 	}
 
-	sourceFile, targetFile := os.Args[1], os.Args[2]
+	switch *paletteName {
+	case "pepto":
+		palette = gfx.Pepto
+	case "colodore":
+		palette = gfx.Colodore
+	case "vice":
+		palette = gfx.Vice
+	case "levy":
+		palette = gfx.Levy
+	default:
+		usage()
+	}
+
+	sourceFile := flag.Arg(0)
+	targetFile := flag.Arg(1)
 
 	hires := make([]byte, 9194)
 	f, err := os.Open(sourceFile)
@@ -41,26 +65,26 @@ func main() {
 		screen = 8194
 	}
 
-    img := image.NewPaletted(image.Rect(0, 0, 320, 200), gfx.Pepto)
+	img := image.NewPaletted(image.Rect(0, 0, 320, 200), gfx.Pepto)
 
 	for row := 0; row < 25; row++ {
 		for col := 0; col < 40; col++ {
-			scr := hires[screen + row * 40 + col]
+			scr := hires[screen+row*40+col]
 			for y := 0; y < 8; y++ {
-				byte := hires[2 + row * 320 + col * 8 + y]
+				byte := hires[2+row*320+col*8+y]
 				for x := 0; x < 8; x++ {
-					if byte & mask[x] > 0 {
+					if byte&mask[x] > 0 {
 						c = (scr & 0xF0) >> 4
 					} else {
 						c = scr & 0x0F
 					}
-					img.Set(col * 8 + x, row * 8 + y, palette[c])
+					img.Set(col*8+x, row*8+y, palette[c])
 				}
 			}
 		}
 	}
 
-    f, _ = os.OpenFile(targetFile, os.O_WRONLY|os.O_CREATE, 0600)
-    defer f.Close()
-    png.Encode(f, img)
+	f, _ = os.OpenFile(targetFile, os.O_WRONLY|os.O_CREATE, 0600)
+	defer f.Close()
+	png.Encode(f, img)
 }
