9316d65b5ddffb46e531a9cd12c7c6a176834115
Lars Haugseth <git@larshaugseth.com>
2020-10-29 12:56:10 +0100

    Read palette definitions from ~/.config/vic-palettes.json

--- pkg/gfx/palette.go
+++ pkg/gfx/palette.go
@@ -5,9 +5,13 @@ package gfx
 
 import (
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	"image/color"
+	"io/ioutil"
 	"log"
+	"os/user"
+	"path/filepath"
 	"strings"
 )
 
@@ -37,30 +41,20 @@ const (
 )
 
 var PaletteMap = map[string]*Palette{}
-var Colodore, Pepto, Levy, Vice, ViceOld, ViceNew, ViceLars *Palette
 
 func init() {
-	Colodore = MakePalette("colodore",
-		"000000:ffffff:813338:75cec8:8e3c97:56ac4d:2e2c9b:edf171",
-		"8e5029:553800:c46c71:4a4a4a:7b7b7b:a9ff9f:706deb:b2b2b2")
-	Pepto = MakePalette("pepto",
-		"000000:ffffff:68372b:70a4b2:6f3d86:588d43:352879:b8c76f",
-		"6f4f25:433900:9a6759:444444:6c6c6c:9ad284:6c5eb5:959595")
-	Levy = MakePalette("levy",
-		"040204:fcfefc:cc3634:84f2dc:cc5ac4:5cce34:4436cc:f4ee5c",
-		"d47e34:945e34:fc9a94:5c5a5c:8c8e8c:9cfe9c:74a2ec:c4c2c4")
-	Vice = MakePalette("vice",
-		"000000:fdfefc:be1a24:30e6c6:b41ae2:1fd21e:211bae:dff60a",
-		"b84104:6a3304:fe4a57:424540:70746f:59fe59:5f53fe:a4a7a2")
-	ViceOld = MakePalette("vice_old",
-		"000000:d5d5d5:72352c:659fa6:733a91:568d35:2e237d:aeb75e",
-		"774f1e:4b3c00:9c635a:474747:6b6b6b:8fc271:675db6:8f8f8f")
-	ViceNew = MakePalette("vice_new",
-		"000000:ffffff:b85438:8decff:ba56e4:79d949:553ee5:fbff79",
-		"bd7c1b:7e6400:f29580:6f716e:a2a4a1:cdff9d:a18aff:d3d5d2")
-	ViceLars = MakePalette("lars_3.4",
-		"000000:ffffff:a75035:8ad9ec:b450bb:69c860:4f37e0:eafb5c",
-		"ac7607:746000:da8c75:6a6a6a:999999:b3ffab:9480ff:c5c5c5")
+	var paletteConfig map[string]string
+	usr, _ := user.Current()
+	configFile := filepath.Join(usr.HomeDir, ".config/vic-palettes.json")
+	paletteJSON, err := ioutil.ReadFile(configFile)
+	if err != nil {
+		log.Printf("Could not read palette config file", err)
+		return
+	}
+	json.Unmarshal(paletteJSON, &paletteConfig)
+	for name, valueList := range paletteConfig {
+		MakePalette(name, strings.Replace(valueList, ",", ":", -1))
+	}
 }
 
 func (p *Palette) Color(index int) color.Color {
@@ -71,7 +65,7 @@ func PaletteByName(name string) *Palette {
 	palette, ok := PaletteMap[name]
 	if !ok {
 		log.Printf("Invalid palette name %q, defaulting to %q.\n", name, "colodore")
-		return Colodore
+		return PaletteMap["colodore"]
 	}
 	return palette
 }
